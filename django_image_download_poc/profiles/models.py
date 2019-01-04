36f71f444e62a0599a2278bcc9bc5387e53877c6
Patrick Heneghan <discopatrick@gmail.com>
2019-01-04 11:34:38 +0000

    Update model and initial migration

--- django_image_download_poc/profiles/models.py
+++ django_image_download_poc/profiles/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
+from filer.fields.image import FilerImageField
+
+
 class Profile(models.Model):
 
     name = models.CharField(max_length=64)
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
+    filer_image = FilerImageField(null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
