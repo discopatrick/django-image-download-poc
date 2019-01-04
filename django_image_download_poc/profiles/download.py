23e388ae696dfc918ee888ee2ab822587dc80e7b
Patrick Heneghan <discopatrick@gmail.com>
2019-01-04 11:35:56 +0000

    Programatically add Filer Image to FilerImageField

--- django_image_download_poc/profiles/download.py
+++ django_image_download_poc/profiles/download.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse
 from urllib.request import urlretrieve
 
 from django.core.files import File
+from filer.models import Image
 
 from profiles.models import Profile
 
@@ -11,11 +12,22 @@ def download(url):
     path, headers = urlretrieve(url)
     url_path = urlparse(url).path
     original_filename = os.path.basename(url_path)
-    file = File(open(path, 'rb'))
 
-    profile = Profile(name='test')
-    profile.image.save(
-        original_filename,
-        file,
-    )
-    profile.save()
+    with open(path, 'rb') as f:
+
+        file = File(f)
+
+        profile = Profile(name='test')
+        profile.image.save(
+            original_filename,
+            file,
+        )
+        profile.save()
+
+        image = Image.objects.create(
+            original_filename=original_filename,
+            file=file,
+        )
+        image.save()
+        profile.filer_image = image
+        profile.save()
